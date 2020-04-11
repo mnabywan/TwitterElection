a1f2b2ae6f93f9c541902deb68bbc81a2c5de583
mnabywan <47008811+mnabywan@users.noreply.github.com>
2020-04-11 14:42:40 +0200

    Added journalists tweets fetching

--- tweets_by_users.py
+++ tweets_by_users.py
@@ -2,17 +2,8 @@ import tweepy
 import datetime, time
 import sqlite3
 from candidates import *
+from authentication import consumer_key, consumer_secret, access_key, access_secret, auth, api
 
-consumer_key = "***REMOVED***"
-consumer_secret = "***REMOVED***"
-access_key = "***REMOVED***"
-access_secret = "***REMOVED***"
-
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-
-api = tweepy.API(auth)
 
 conn = sqlite3.connect('Twitter.db')
 c = conn.cursor()
@@ -26,6 +17,29 @@ c = conn.cursor()
 # );"""
 # c.execute(table_create)
 # conn.commit()
+
+def get_tweets_by_hashtag(api, hashtag, table_name, date_until=None, date_since=None):
+    print(hashtag)
+    for tweet in tweepy.Cursor(api.search,
+                               q= hashtag,
+                                since = date_since,
+                                until = date_until,
+                                rpp=10,
+                                count= 10
+                               ).items():
+        if 'RT @' not in tweet.text:
+            id = tweet.id
+            is_tweet_in_base = "SELECT tweet_id from {} where tweet_id = {}".format(table_name, id)
+            c.execute(is_tweet_in_base)
+            id_in_base = c.fetchall()
+            if not id_in_base:
+                author_name = tweet.author.name
+                print(tweet.created_at)
+                str = "INSERT INTO {}(tweet_id, hashtag) VALUES ({}, \"{}\")"
+                str = str.format(table_name, id, hashtag)
+                #print(str)
+                c.execute(str)
+                conn.commit()
 
 
 def get_tweets_by_user(api, username, candidate):
@@ -61,11 +75,42 @@ def get_tweets_by_user(api, username, candidate):
              time.sleep(10)
 
 
+def get_tweets_by_journalist_account(api, table_name, journalist):
+    print(table_name)
+    print(journalist)
+    page = 1
+    end = False
 
-if __name__ == '__main__':
-    accounts_list = ['michalkobosko']
+    while True:
+        tweets = api.user_timeline(journalist, page=page)
+        for tweet in tweets:
+            if (datetime.datetime.now() - tweet.created_at).days  <= 31:
+                if 'RT @' not in tweet.text and any(word in tweet.text for word in ELECTION_KEYWORDS):
+                    ''' Handle unique  '''
+                    id = tweet.id
+                    is_tweet_in_base = "SELECT tweet_id from journalist_tweets where tweet_id = {}".format(id)
+                    c.execute(is_tweet_in_base)
+                    id_in_base = c.fetchall()
+                    if not id_in_base:
+                        #author_name = tweet.author.name
+                        print(tweet.created_at)
+                        str = "INSERT INTO journalist_tweets(tweet_id, author_name) VALUES ({}, \"{}\")"
+                        str = str.format(id, journalist)
+                        print(str)
+                        c.execute(str)
+                        conn.commit()
+            else:
+                end = True
+                return
 
-    print(accounts_list)
+        if not end:
+             page = page + 1
+            #time.sleep(10)
+
+
+def get_tweets_by_candidates_accounts():
+    accounts_list = DUDA_ACCOUNTS + KIDAWA_ACCOUNTS + KOSINIAK_HASHTAGS \
+                    + BIEDRON_HASHTAGS + HOLOWNIA_ACCOUNTS + BOSAK_HASHTAGS
     for user in accounts_list:
         if user in DUDA_ACCOUNTS:
             candidate = DUDA
@@ -81,3 +126,47 @@ if __name__ == '__main__':
             candidate = HOLOWNIA
 
         get_tweets_by_user(api, user, candidate)
+
+
+def get_tweets_by_hashtag_start():
+    table_name = "election_tweets"
+    for hashtag in ELECTION_HASHTAGS:
+        get_tweets_by_hashtag(api, hashtag, table_name=table_name, date_until="2020-04-06")
+
+
+def get_tweets_by_candidates_hashtags_start():
+    hashtag_list =  KIDAWA_HASHTAGS + KOSINIAK_HASHTAGS +\
+                    BIEDRON_HASHTAGS + HOLOWNIA_HASHTAGS + BOSAK_HASHTAGS
+    for hashtag in hashtag_list:
+        if hashtag in DUDA_HASHTAGS:
+            table_name = "duda_hashtags"
+        elif hashtag in KIDAWA_HASHTAGS:
+            table_name = "kidawa_hashtags"
+        elif hashtag in KOSINIAK_HASHTAGS:
+            table_name = "kosiniak_hashtags"
+        elif hashtag in BIEDRON_HASHTAGS:
+            table_name = "biedron_hashtags"
+        elif hashtag in HOLOWNIA_HASHTAGS:
+            table_name = "holownia_hashtags"
+        elif hashtag in BOSAK_HASHTAGS:
+            table_name = "bosak_hashtags"
+        get_tweets_by_hashtag(api, hashtag, table_name=table_name, date_until="2020-04-05")
+
+
+
+def get_tweets_by_journalists_account_start():
+    table_name = "journalist_tweets"
+    for journalist in JOURNALISTS_ACCOUNTS:
+        get_tweets_by_journalist_account(api, table_name, journalist)
+
+if __name__ == '__main__':
+    t = api.get_status(1248538479048491008)
+    print(t.text)
+    print(t.author)
+    print(t.author.screen_name)
+    #get_tweets_by_hashtag_start()
+    #get_tweets_by_candidates_hashtags_start()
+    get_tweets_by_journalists_account_start()
+
+
+
