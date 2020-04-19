b08c4a8e6884b6b77aeeacf39ca8e673358414d8
mnabywan <47008811+mnabywan@users.noreply.github.com>
2020-04-19 14:50:10 +0200

    Some authentication update

--- authentication.py
+++ authentication.py
@@ -1,12 +1,13 @@
 import tweepy
+#
 
-consumer_key = "***REMOVED***"
-consumer_secret = "***REMOVED***"
-access_key = "***REMOVED***"
-access_secret = "***REMOVED***"
 
+consumer_key =
+consumer_secret =
+access_key =
+access_secret =
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
