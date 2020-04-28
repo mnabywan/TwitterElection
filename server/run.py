e77922e511a4aeea35bed9461dc20f83671f2571
gregxsunday <gregxsunday@gmail.com>
2020-04-28 10:17:31 +0200

    added chart with likes and rts to web app

--- server/run.py
+++ server/run.py
@@ -6,13 +6,20 @@ from os import path
 scripts_path = path.abspath('../scripts')
 modules_directories.append(scripts_path)
 import authentication
+import analysis
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def template_test():
+def index():
     return render_template('home.html')
+
+
+@app.route("/charts/likes_rts")
+def likes_rts_chart(chart):
+    return render_template('charts.html', chart='/static/charts/likes_and_rts.svg', title='Wykres Liczby polubień i reetweetów wpisów kandydatów')
+
 
 
 if __name__ == '__main__':
