11a73042f0a32f3bd2df719dfe91eac2a1781b2e
gregxsunday <gregxsunday@gmail.com>
2020-06-01 17:55:27 +0200

    added charts with tags

--- server/run.py
+++ server/run.py
@@ -7,8 +7,15 @@ scripts_path = path.abspath('../scripts')
 modules_directories.append(scripts_path)
 import authentication
 import analysis
+from candidates import CANDIDATES
 
 app = Flask(__name__)
+
+def validate_candidate(candidate):
+    if candidate.capitalize() in CANDIDATES:
+        return True
+    return False
+
 
 
 @app.route("/")
@@ -48,31 +55,70 @@ def favourite_count():
 
 @app.route("/wordclouds/biedron_words")
 def biedron_wordcloud():
-    return render_template('charts.html', chart='/static/wordclouds/words/Biedron_common_words.png', title='Wordcloud - Biedron')
+    return render_template('charts.html', chart='/static/wordclouds/words/Biedron_common_words.png', title='Wordcloud słów - Biedron')
 
 @app.route("/wordclouds/bosak_words")
 def bosak_wordcloud():
-    return render_template('charts.html', chart='/static/wordclouds/words/Bosak_common_words.png', title='Wordcloud - Bosak')
+    return render_template('charts.html', chart='/static/wordclouds/words/Bosak_common_words.png', title='Wordcloud słów - Bosak')
 
 @app.route("/wordclouds/duda_words")
 def duda_wordcloud():
-    return render_template('charts.html', chart='/static/wordclouds/words/Duda_common_words.png', title='Wordcloud - Duda')
+    return render_template('charts.html', chart='/static/wordclouds/words/Duda_common_words.png', title='Wordcloud słów - Duda')
 
 @app.route("/wordclouds/holownia_words")
 def holownia_wordcloud():
-    return render_template('charts.html', chart='/static/wordclouds/words/Holownia_common_words.png', title='Wordcloud - Holownia')
+    return render_template('charts.html', chart='/static/wordclouds/words/Holownia_common_words.png', title='Wordcloud słów - Holownia')
 
 @app.route("/wordclouds/kidawa_words")
 def kidawa_wordcloud():
-    return render_template('charts.html', chart='/static/wordclouds/words/Kidawa_common_words.png', title='Wordcloud - Kidawa')
+    return render_template('charts.html', chart='/static/wordclouds/words/Kidawa_common_words.png', title='Wordcloud słów - Kidawa')
 
 @app.route("/wordclouds/kosiniak_words")
 def kosiniak_wordcloud():
-    return render_template('charts.html', chart='/static/wordclouds/words/Kosiniak_common_words.png', title='Wordcloud - Kosiniak')
+    return render_template('charts.html', chart='/static/wordclouds/words/Kosiniak_common_words.png', title='Wordcloud słów - Kosiniak')
 
 @app.route("/wordclouds/all_words")
 def all_wordcloud():
     return render_template('charts.html', chart='/static/wordclouds/words/all_common_words.png', title='Wordcloud - wszystkie słowa')
+
+@app.route("/wordclouds/biedron_tags")
+def biedron_wordcloud_tags():
+    return render_template('charts.html', chart='/static/wordclouds/tags/Biedron_tags.png', title='Wordcloud tagów - Biedron')
+
+@app.route("/wordclouds/bosak_tags")
+def bosak_wordcloud_tags():
+    return render_template('charts.html', chart='/static/wordclouds/tags/Bosak_tags.png', title='Wordcloud tagów - Bosak')
+
+@app.route("/wordclouds/duda_tags")
+def duda_wordcloud_tags():
+    return render_template('charts.html', chart='/static/wordclouds/tags/Duda_tags.png', title='Wordcloud tagów - Duda')
+
+@app.route("/wordclouds/holownia_tags")
+def holownia_wordcloud_tags():
+    return render_template('charts.html', chart='/static/wordclouds/tags/Holownia_tags.png', title='Wordcloud tagów - Holownia')
+
+@app.route("/wordclouds/kidawa_tags")
+def kidawa_wordcloud_tags():
+    return render_template('charts.html', chart='/static/wordclouds/tags/Kidawa_tags.png', title='Wordcloud tagów - Kidawa')
+
+@app.route("/wordclouds/kosiniak_tags")
+def kosiniak_wordcloud_tags():
+    return render_template('charts.html', chart='/static/wordclouds/tags/Kosiniak_tags.png', title='Wordcloud tagów - Kosiniak')
+
+@app.route("/wordclouds/all_tags")
+def all_wordcloud_tags():
+    return render_template('charts.html', chart='/static/wordclouds/tags/all_common_tags.png', title='Wordcloud - wszystkie tagi')
+
+@app.route("/charts/tags/<candidate>")
+def tags(candidate):
+    if validate_candidate(candidate) or candidate == 'all_common':
+        candidate = candidate.capitalize()
+        title = f'Najpopularniejsze tagi - {candidate}'
+        if candidate == 'All_common':
+            title = 'Najpopularniejsze tagi - wszyscy kandydaci'
+        return render_template('charts.html', chart=f'/static/charts/tags/{candidate}_tags.png', title=title)
+    else:
+        return 'Invalid candidate', 404
 
 if __name__ == '__main__':
     app.run(debug=True)
